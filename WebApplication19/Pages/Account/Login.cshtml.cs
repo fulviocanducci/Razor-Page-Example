ecac24c8f12e3ff77bae25e1de0ec6ca6d91bff6
fulviocanducci <fulviocanducci@hotmail.com>
2017-12-10 23:27:12 -0200

    Add project files.

---
+++ WebApplication19/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication19.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+
+            await CreateCookie("fulviocanducci@hotmail.com");
+            return RedirectToPage("/admin/index");
+        }
+
+
+        public async Task CreateCookie (string email)
+        {
+            var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, email)
+                };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+        }
+    }
+}
---
+++ WebApplication19/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication19.Pages.Admin
+{
+    //[Authorize]
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+
+        
+    }
+}
